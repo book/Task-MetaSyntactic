4d9152a764698966b2a8aece54f1acc58bddfe2a
Philippe Bruhat (BooK) <book@cpan.org>
2012-05-08 02:35:16 +0200

    include Acme::MetaSyntactic::Themes in the bundle, and describe the changes

--- lib/Bundle/MetaSyntactic.pm
+++ lib/Bundle/MetaSyntactic.pm
@@ -1,6 +1,6 @@
 package Bundle::MetaSyntactic;
 
-our $VERSION = '1.01';
+our $VERSION = '1.020';
 
 'whack_eth';
 
@@ -12,29 +12,40 @@ Bundle::MetaSyntactic - All Acme::MetaSyntactic themes
 
 =head1 SYNOPSIS
 
-C<perl -MCPAN -e 'install Bundle::MetaSyntactic'>
+    perl -MCPAN -e 'install Bundle::MetaSyntactic'
 
 =head1 DESCRIPTION
 
-C<Acme::MetaSyntactic> is a module designed to fill all your metasyntactic
+L<Acme::MetaSyntactic> is a module designed to fill all your metasyntactic
 needs. There was a weekly update with new themes on CPAN between January
 15, 2005 and November 6, 2006.
 
 Since version 0.99, the weekly uploads of the module have stopped,
-while I take some rest. I'm also looking for a better way to distribute
-the core modules and the themes themselves. The current idea is to have
-theme packs.
+while I took some rest. I've also been looking for a better way to distribute
+the core modules and the themes themselves.
 
-However, some users of C<Acme::MetaSyntactic> desperately needed new
+However, some users of L<Acme::MetaSyntactic> desperately needed new
 themes to be added, and grew tired of waiting for me. So, in a JFDI way,
 they started to upload their own themes on CPAN.
 
-This bundle brings you all the official and not-yet-official
-C<Acme::MetaSyntactic> themes, in a single command (see the L<SYNOPSIS>).
+On May 7, 2012, after a hiatus of five and a half years,
+L<Acme::MetaSyntactic> is back, in a shiny version 1.000.
+The main C<Acme-MetaSyntactic> distribution contains the core modules
+and a single theme, while all "historical" themes now live in the
+C<Acme-MetaSyntactic-Themes> distribution.
+
+The L<Test::MetaSyntactic> module included in the main distribution
+aims at simplifying the test and distribution of new themes by
+individual authors.
+
+This bundle brings you all the official and not-so-official
+L<Acme::MetaSyntactic> themes, in a single command (see the L<SYNOPSIS>).
 
 =head1 CONTENTS
 
-Acme::MetaSyntactic 0.99 - Themed metasyntactic variables names
+Acme::MetaSyntactic 1.000 - Themed metasyntactic variables names
+
+Acme::MetaSyntactic::Themes 1.000 - The original Acme::MetaSyntactic themes
 
 Acme::MetaSyntactic::buzzwords - Pointy-haired boss lingo (JQUELIN)
 
@@ -48,9 +59,11 @@ Acme::MetaSyntactic::vim - The vim theme (ELLIOTJS)
 
 Philippe Bruhat (BooK), <book@cpan.org>
 
-=head1 COPYRIGHT AND LICENSE
+=head1 COPYRIGHT
 
-Copyright 2005-2007 Philippe Bruhat (BooK), All Rights Reserved.
+Copyright 2005-2012 Philippe Bruhat (BooK), All Rights Reserved.
+
+=head1 LICENSE
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
