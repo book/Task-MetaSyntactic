1849f60816bbceab613efddbfc39570c17910128
Philippe Bruhat (BooK) <book@cpan.org>
2017-02-22 23:06:49 +0100

    Update copyright date

--- lib/Task/MetaSyntactic.pm
+++ lib/Task/MetaSyntactic.pm
@@ -260,7 +260,7 @@ Philippe Bruhat (BooK), <book@cpan.org>.
 
 =head1 COPYRIGHT
 
-Copyright 2005-2016 Philippe Bruhat (BooK), All Rights Reserved.
+Copyright 2005-2017 Philippe Bruhat (BooK), All Rights Reserved.
 
 =head1 LICENSE
 
